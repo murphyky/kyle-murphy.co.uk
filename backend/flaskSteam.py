f2592731ec753336990d46a8b7e7a4d672643a92
Kyle <kyle-murphy89@live.com>
2016-01-31 16:16:05 +0000

    updates

---
+++ backend/flaskSteam.py
@@ -0,0 +1,28 @@
+#from urllib import request, parse
+import requests
+import os
+
+steam_api = 'http://api.steampowered.com/'
+
+from flask import Flask, request, make_response
+app = Flask(__name__)
+
+@app.route('/api/get_user/<user_id>')
+def get_user(user_id):
+
+	error = None
+
+	url = steam_api+'ISteamUser/GetPlayerSummaries/v0002/'
+	
+	params = {
+		'key': app.config['STEAM_KEY'],
+		'steamids' : user_id
+	}
+
+	resp = make_response(requests.get(url, params).text)
+	
+	return resp
+
+if __name__ == '__main__':
+	app.config.from_object('config')
+	app.run(debug=True)
